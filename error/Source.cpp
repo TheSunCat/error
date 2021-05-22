4b387b7b02039adff438603d57caede9f0c64be2
TheSunCat <thesuncat123@gmail.com>
2021-05-22 16:38:09 +0200

    switch to C++20

--- error/Source.cpp
+++ error/Source.cpp
@@ -11,15 +11,17 @@ std::vector<int> yPos;
 int id = 0;
 static void CALLBACK WinEventProc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime)
 {
+	std::cout << "running callback! \n";
+	
 	int MsgBox_X = xPos[id];
 	int MsgBox_Y = yPos[id];
-	
+
 	SetWindowPos(hwnd, nullptr, MsgBox_X, MsgBox_Y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 
 	id++;
 }
 
-void error(HWND hwnd, LPCWSTR msg, int X, int Y)
+void error(HWND hwnd, LPCWSTR message, int X, int Y)
 {
 	HWINEVENTHOOK hHook = SetWinEventHook(EVENT_OBJECT_CREATE, EVENT_OBJECT_CREATE, nullptr,
 		&WinEventProc, GetCurrentProcessId(), GetCurrentThreadId(), WINEVENT_INCONTEXT);
@@ -28,10 +30,12 @@ void error(HWND hwnd, LPCWSTR msg, int X, int Y)
 	xPos.push_back(X);
 	yPos.push_back(Y);
 	
-	MessageBox(hwnd, msg,
+	MessageBox(hwnd, message,
 		L"Microsoft Windows", MB_OK | MB_ICONERROR);
-
+	
 	if (hHook) UnhookWinEvent(hHook);
+
+	std::cout << "id: " << id << '\n';
 }
 
 int main(int argc, const char* argv)
@@ -49,22 +53,22 @@ int main(int argc, const char* argv)
 	int center_x = width / 2;
 	int center_y = height / 2;
 
-	std::vector<std::thread> threads;
+
+	{ // scope the threads so they all end before Done is printed
+		std::vector<std::jthread> threads;
+
+		std::cout << "Starting..." << std::endl;
+		for (int i = 0; i < 5; i++) {
+
+			int boxPos_x = center_x + 30 * i;
+			int boxPos_y = center_y + 250 * i;
+
+			threads.emplace_back(std::jthread(error, hwnd, L"Windows was not installed correctly. Please reinstall Windows.\nError 4 (Windows error 096)", boxPos_x, boxPos_y));
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(250));
+		}
+	}
 	
-	std::cout << "Starting..." << std::endl;
-	for (int i = 0; i < 5; i++) {
-		//std::stringstream ss(); ss << i;
-		
-		threads.emplace_back(std::thread(error, hwnd, L"Windows was not installed correctly. Please reinstall Windows.\nError 4 (Windows error 096)", center_x + 30*i, center_y + 25*i));
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(250));
-	}
-
-	for(auto& t : threads)
-	{
-		t.join();
-	}
-
 	std::cout << "Done!";
 	getchar();
 	
